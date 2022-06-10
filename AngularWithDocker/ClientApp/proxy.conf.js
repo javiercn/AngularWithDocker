47b55c6dc0b8990efd10383c3b110c65acdead64
jacalvar <jacalvar@microsoft.com>
2022-06-10 15:21:08 +0200

    Add project files.

---
+++ AngularWithDocker/ClientApp/proxy.conf.js
@@ -0,0 +1,18 @@
+const { env } = require('process');
+
+const target = 'https://localhost:49153';
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/weatherforecast",
+   ],
+    target: target,
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
+  }
+]
+
+module.exports = PROXY_CONFIG;
